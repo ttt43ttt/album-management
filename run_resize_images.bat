56ffec593714e1f9ec325c907594fddbe90bac5f
Wilson Tian <wilson.tian@perkinelmer.com>
2020-02-15 18:14:26 +0800

    ignore userdata folder

--- run_resize_images.bat
+++ run_resize_images.bat
@@ -1,4 +1,4 @@
 set ANACONDA_HOME=C:\apps\Anaconda3
 call %ANACONDA_HOME%\Scripts\activate.bat %ANACONDA_HOME%\envs\py36_x86
 
-python resize_images.py -i input -o dataset -s 800
+python resize_images.py -i userdata -o dataset -s 800
