cbb9d40ddb9f46e4de7aa816d0972c4ce47f5a2c
Wilson Tian <wilson.tian@perkinelmer.com>
2020-02-17 00:27:05 +0800

    show person info on top of photos

--- webui/src/pages/PersonPhotosPage/index.js
+++ webui/src/pages/PersonPhotosPage/index.js
@@ -3,10 +3,12 @@ import * as R from 'ramda';
 import { connect } from 'dva';
 import { Button, Spin, Pagination, Menu, Dropdown, Icon } from 'antd';
 import PhotoGallery from '@/components/PhotoGallery';
+import InputToggle from '@/components/InputToggle';
 import LinkPhotosModal from '@/pages/PersonsPage/LinkPhotosModal';
 import styles from './style.less';
 
-@connect(({ personPhoto, loading }) => ({
+@connect(({ person, personPhoto, loading }) => ({
+  persons: person.list,
   query: personPhoto.query,
   photos: personPhoto.list,
   meta: personPhoto.meta,
@@ -22,6 +24,10 @@ class Page extends React.Component {
   }
 
   componentDidMount() {
+    if (this.props.persons.length === 0) {
+      this.props.dispatch({ type: 'person/listPersons' });
+    }
+
     this.props.dispatch({
       type: 'personPhoto/listPhotos',
       personId: this.getPersonId(),
@@ -29,7 +35,10 @@ class Page extends React.Component {
     });
   }
 
-  getPersonId = () => R.path(['match', 'params', 'id'], this.props);
+  getPersonId = () => {
+    const id = R.path(['match', 'params', 'id'], this.props);
+    return Number(id);
+  };
 
   onPageChange = (pageNumber, pageSize) => {
     const { query } = this.props;
@@ -41,9 +50,10 @@ class Page extends React.Component {
   };
 
   render() {
-    const { query, photos, meta, isLoading } = this.props;
+    const { persons, query, photos, meta, isLoading } = this.props;
     const { selectedKeys, linkPhotosModal } = this.state;
     const { pageNumber, pageSize } = query;
+    const personId = this.getPersonId();
 
     const menu = (
       <Menu
@@ -52,21 +62,13 @@ class Page extends React.Component {
             case 'remove':
               this.props.dispatch({
                 type: 'personPhoto/linkPhotosToPerson',
-                payload: {
-                  photoIds: selectedKeys,
-                  personId: this.getPersonId(),
-                  newPersonId: null,
-                },
+                payload: { photoIds: selectedKeys, personId, newPersonId: null },
               });
               break;
 
             case 'linkToOthers':
               this.setState({
-                linkPhotosModal: {
-                  visible: true,
-                  personId: this.getPersonId(),
-                  photoIds: selectedKeys,
-                },
+                linkPhotosModal: { visible: true, personId, photoIds: selectedKeys },
               });
               break;
             default:
@@ -79,15 +81,37 @@ class Page extends React.Component {
       </Menu>
     );
 
+    const person = R.find(p => p.id === personId, persons);
+
     return (
       <div>
         <div className={styles.actionBar}>
+          {person && (
+            <div className={styles.personInfo}>
+              <div>
+                <InputToggle
+                  value={person.name}
+                  placeholder="设置名字"
+                  onChange={newName =>
+                    this.props.dispatch({
+                      type: 'person/renamePerson',
+                      personId: person.id,
+                      name: newName,
+                    })
+                  }
+                />
+              </div>
+              <div style={{ color: 'gray', fontSize: 'smaller' }}>{person.photoCount}张照片</div>
+            </div>
+          )}
+
           <Dropdown overlay={menu} disabled={selectedKeys.length < 1}>
             <Button>
               不是此人 <Icon type="down" />
             </Button>
           </Dropdown>
         </div>
+
         <Spin spinning={isLoading}>
           <PhotoGallery
             photos={photos}
