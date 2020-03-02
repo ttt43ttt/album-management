e21103da43ed72a2586cc79f0c7fa3718259c508
Wilson Tian <wilson.tian@perkinelmer.com>
2020-03-02 12:05:50 +0800

    implement more accurate file event handler

--- services/photo-album/watch_folder.py
+++ services/photo-album/watch_folder.py
@@ -1,11 +1,13 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from imutils import paths
 
 from logger import get_logger
 import settings
-from photos import reload_photo
+from photos import reload_photo, reload_photos
 
 logger = get_logger()
+
 
 def start_watch_files():
     folder = settings.USER_PHOTO_HOME
@@ -21,6 +23,29 @@ class FileChangeEventHandler(FileSystemEventHandler):
         pass
 
     def on_any_event(self, event):
-        logger.info(f"{event.event_type}: {event.src_path}")
-        if not event.is_directory:
+        logger.info(event)
+
+    def on_created(self, event):
+        if event.is_directory:
+            pass
+        else:
             reload_photo(event.src_path)
+
+    def on_deleted(self, event):
+        if event.is_directory:
+            pass
+        else:
+            reload_photo(event.src_path)
+
+    def on_modified(self, event):
+        if event.is_directory:
+            pass
+        else:
+            reload_photo(event.src_path)
+
+    def on_moved(self, event):
+        if event.is_directory:
+            pass
+        else:
+            reload_photo(event.src_path)
+            reload_photo(event.dest_path)
